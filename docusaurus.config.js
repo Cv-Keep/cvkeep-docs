26562f6a7bb022e84efc3686277160d04ce04074
Felippe Regazio <felippe.moraes@zoho.com>
2021-10-04 23:49:40 -0300

    deployment config ready

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   favicon: 'img/favicon.ico',
   organizationName: 'cvkeep',
   projectName: 'cvkeep-docs',
+  trailingSlash: false,
 
   presets: [
     [
